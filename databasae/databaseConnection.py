cd01b63c0105115d2c1d489b3ae4731f50520480
hechun2024 <Chun.He@metropolia.fi>
2024-10-06 16:46:00 +0300

    Update databaseConnection.py

--- databasae/databaseConnection.py
+++ databasae/databaseConnection.py
@@ -1,12 +1,49 @@
-import mysql.connector
+#create database for game
+create database vaccine_game;
+use vaccine_game;
+show databases;
+use vaccine_game;
+show tables;
 
-connection = mysql.connector.connect(
-    user = 'sailesh',
-    password = 'sailesh1103',
-    host = 'localhost',
-    port = '3306',
-    database = 'flight_game',
-    autocommit = True,
-    charset = 'utf8mb4',
-    collation = 'utf8mb4_unicode_ci'
-)
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE player_data;
+DROP TABLE goal_elements;
+DROP TABLE random_airports;
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+CREATE TABLE game (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    money DECIMAL(10, 2) NOT NULL,
+    player_range DECIMAL(10, 2) NOT NULL,
+    location VARCHAR(10) NOT NULL,
+    screen_name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE element (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(1) NOT NULL,  -- A, B, C, D
+    total_quantity INT NOT NULL  -- A:4, B:3, C:3, D:2
+);
+
+INSERT INTO element (name, total_quantity) VALUES
+('A', 4),
+('B', 3),
+('C', 3),
+('D', 2)
+ON DUPLICATE KEY UPDATE name = VALUES(name), total_quantity = VALUES(total_quantity);
+
+CREATE TABLE port_contents (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  game_id INT NOT NULL,
+  airport VARCHAR(10) NOT NULL,
+  content_type ENUM('element', 'lucky_box') NOT NULL,
+  content_value CHAR(1) DEFAULT NULL,
+  found TINYINT(1) DEFAULT 0
+ );
+ALTER TABLE port_contents
+ADD CONSTRAINT fk_game_id
+FOREIGN KEY (game_id) REFERENCES game(id);
+
+
